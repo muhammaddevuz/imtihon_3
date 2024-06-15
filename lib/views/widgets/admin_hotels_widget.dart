6315df06e4437beb8de2836ea0b93f9ec77e6f2e
Isroil <isroilkhasanov98@gmail.com>
2024-06-15 20:56:05 +0500

    ozgarish3

--- lib/views/widgets/admin_hotels_widget.dart
+++ lib/views/widgets/admin_hotels_widget.dart
@@ -25,88 +25,115 @@ class _HotelsWidgetState extends State<HotelsWidget> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: 10, top: widget.index == 0 ? 15 : 0),
-      padding: const EdgeInsets.all(5),
+      padding: EdgeInsets.only(top: 5.h,bottom: 5.h),
       width: 343.w,
-      height: 172.h,
+      height: 200.h,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: Colors.white),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
       child: Row(
         children: [
           Container(
             width: 114.w,
             height: double.infinity,
-            child: Image(
-              image: NetworkImage(widget.hotels[widget.index].imageUrl[0]),
-              fit: BoxFit.cover,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.r),
+                bottomLeft: Radius.circular(10.r),
+              ),
+              image: DecorationImage(
+                image: NetworkImage(widget.hotels[widget.index].imageUrl[0]),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          SizedBox(
-            width: 12.w,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.hotels[widget.index].hotelName,
-                style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w700),
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Text(
-                widget.hotels[widget.index].location,
-                style: TextStyle(fontSize: 14.sp, color: Colors.grey),
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    color: Colors.amber,
-                  ),
-                  Text(
-                    widget.hotels[widget.index].rating[0].toString(),
-                    style: TextStyle(fontSize: 14.sp, color: Colors.grey),
-                  ),
-                  SizedBox(width: 5,),
-                  Text(
-                    '(${widget.hotels[widget.index].description})',
-                    style: TextStyle(fontSize: 14.sp, color: Colors.grey),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),Row(children: [...List.generate(widget.hotels[widget.index].amenities.length, (int index){
-                return SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Text(
-                    widget.hotels[widget.index].amenities[index],
-                    style: TextStyle(fontSize: 14.sp, color: Colors.grey),
-                  ),
-                );
-              }),],),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: widget.onEditPressed,
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.blue,
+          SizedBox(width: 12.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.hotels[widget.index].hotelName,
+                  style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 5.h),
+                Text(
+                  widget.hotels[widget.index].location,
+                  style: TextStyle(fontSize: 16.sp,),
+                ),
+                SizedBox(height: 5.h),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    Text(
+                      widget.hotels[widget.index].rating[0].toString(),
+                      style: TextStyle(fontSize: 14.sp),
+                    ),
+                    SizedBox(width: 5),
+                    Expanded(
+                      child: Text(
+                        '(${widget.hotels[widget.index].description})',
+                        style: TextStyle(fontSize: 14.sp),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5.h),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children:
+                      List.generate(
+                        widget.hotels[widget.index].amenities.length,
+                            (int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Text(
+                              "${widget.hotels[widget.index].amenities[index]}",
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
-                  IconButton(
-                    onPressed: widget.onDeletePressed,
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.red,
+                ),
+                SizedBox(height: 5,),
+                Text("Space rooms: ${widget.hotels[widget.index].spaceRooms[0].toString()}"),
+                SizedBox(height: 5.h),
+                Text(
+                  '\$${widget.hotels[widget.index].price}/night',
+                  style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: widget.onEditPressed,
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Colors.blue,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    IconButton(
+                      onPressed: widget.onDeletePressed,
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/views/widgets/edit_hotel_dialog.dart
+++ lib/views/widgets/edit_hotel_dialog.dart
@@ -4,16 +4,11 @@ import 'package:imtihon3/controllers/admin/admin_controller.dart';
 import '../../models/hotel.dart';
 
 class EditHotelDialog extends StatefulWidget {
-  final Hotel  hotel;
+  final Hotel hotel;
   final Function refresh;
 
-
-  const EditHotelDialog({
-    super.key,
-    required this.hotel,
-    required this.refresh
-
-  });
+  const EditHotelDialog(
+      {super.key, required this.hotel, required this.refresh});
 
   @override
   State<EditHotelDialog> createState() => _EditHotelDialogState();
@@ -33,26 +28,19 @@ class _EditHotelDialogState extends State<EditHotelDialog> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   AdminController adminController = AdminController();
 
-
-
-
-  // void addHotel(amenities,  comment,  hotelName,  description,  imageUrl, price, rating, spaceRooms) async{
-  //   await adminController.addHotel(amenities: amenities, comment: comment, hotelName: hotelName, description: description, imageUrl: imageUrl, price: price, rating: rating, rooms: spaceRooms);
-  // }
-
   @override
   void initState() {
     super.initState();
 
-      amenities = widget.hotel.amenities;
-      comment = widget.hotel.comment;
-      description = widget.hotel.description;
-      hotelName = widget.hotel.hotelName;
-      imageUrl = widget.hotel.imageUrl;
-      price = widget.hotel.price;
-      rating = widget.hotel.rating;
-      spaceRooms = widget.hotel.spaceRooms;
-      location = widget.hotel.location;
+    amenities = widget.hotel.amenities;
+    comment = widget.hotel.comment;
+    description = widget.hotel.description;
+    hotelName = widget.hotel.hotelName;
+    imageUrl = widget.hotel.imageUrl;
+    price = widget.hotel.price;
+    rating = widget.hotel.rating;
+    spaceRooms = widget.hotel.spaceRooms;
+    location = widget.hotel.location;
   }
 
   @override
@@ -126,24 +114,7 @@ class _EditHotelDialogState extends State<EditHotelDialog> {
                 },
               ),
               TextFormField(
-                //initialValue: widget.hotel.description,
-                initialValue: comment.join(','),
-                decoration: const InputDecoration(labelText: 'Comment'),
-                validator: (String? value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Enter something!';
-                  }
-                  return null;
-                },
-                onSaved: (String? newValue) {
-                  if (newValue != null) {
-                    comment = newValue.split(',');
-                  }
-                },
-              ),
-              TextFormField(
                 keyboardType: TextInputType.number,
-                //initialValue: widget.hotel.description,
                 initialValue: price.toString(),
                 decoration: const InputDecoration(labelText: 'Price'),
                 validator: (String? value) {
@@ -160,7 +131,7 @@ class _EditHotelDialogState extends State<EditHotelDialog> {
               ),
               TextFormField(
                 initialValue: rating.join(','),
-                decoration: const InputDecoration(labelText: 'Comment'),
+                decoration: const InputDecoration(labelText: 'Rating'),
                 validator: (String? value) {
                   if (value == null || value.trim().isEmpty) {
                     return 'Enter something!';
